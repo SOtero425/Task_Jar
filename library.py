020d535cb0036f4a28e9db8dc6ea30bbcbf85d93
Samuel Otero <99850526+SOtero425@users.noreply.github.com>
2022-06-19 00:34:07 -0500

    Upload library.py
    library.py will be the collection of functions, classes, and associated methods that will underpin the "Honey Dew Task Jar" program.

---
+++ library.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 18 18:14:18 2022
+
+@author: soter
+"""
+
+# import numpy as np
+
+
+class Task():
+    def __init__(self, string):
+        self.task = str(string)
